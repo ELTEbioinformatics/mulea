0f60f9f95c0fbb2807432743490b5bcc5d2a8420
cezary turek <cezary.turek@gmail.com>
2024-08-16 22:38:37 +0200

    Seed propagation with test.

--- tests/testthat/testPermTest.R
+++ tests/testthat/testPermTest.R
@@ -27,3 +27,28 @@ test_that("set.based.enrichment.test.", {
   
   testthat::expect_gt(res[["FDR"]][1], 0)
 })
+
+
+test_that("do_the_simulation", {
+  steps <- 100
+  DB <- create_random_db()
+  pool <- unique(unlist(DB))
+  select <- c("el_4", "el_5", "el_6")
+  nthread <- 2
+  list_of_all_genes <- unique(c(unlist(DB), pool))
+  res_1 <- mulea:::do_the_simulation(list_of_all_genes = list_of_all_genes, 
+                                   pool = pool, 
+                                   select = select,
+                                   DB = DB, 
+                                   steps = steps, 
+                                   nthread = nthread, 
+                                   random_seed = 123)
+  res_2 <- mulea:::do_the_simulation(list_of_all_genes = list_of_all_genes, 
+                                   pool = pool, 
+                                   select = select,
+                                   DB = DB, 
+                                   steps = steps, 
+                                   nthread = nthread, 
+                                   random_seed = 123)
+  testthat::expect_equal(res_1, res_2)
+})
